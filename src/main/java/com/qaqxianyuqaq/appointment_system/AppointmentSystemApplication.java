4cca2e04134385fbff497d89410a234d47199a98
弄臣 <33161276@qq.com>
2026-02-15 12:09:13 +0800

    啊对吗？哦不对不对。。。啊对的对的！

--- src/main/java/com/qaqxianyuqaq/appointment_system/AppointmentSystemApplication.java
+++ src/main/java/com/qaqxianyuqaq/appointment_system/AppointmentSystemApplication.java
@@ -3,9 +3,10 @@ package com.qaqxianyuqaq.appointment_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@Enablecaching
+@EnableCaching
 public class AppointmentSystemApplication {
 
     public static void main(String[] args) {
